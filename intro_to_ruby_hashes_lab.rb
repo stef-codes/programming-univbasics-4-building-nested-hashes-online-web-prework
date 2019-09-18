88f98a5c3977b3fb5f270d9407deb8056282bd95
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-18 00:12:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,10 +25,10 @@ def monopoly_with_third_tier
     monopoly = {:railroads => {
                 :pieces => 4, 
                 :rent_in_dollars => {
-                  :one_piece_owned => ,
-                  :two_pieces_owned => ,
-                  :three_pieces_owned => ,
-                  :four_pieces_owned =>
+                  :one_piece_owned => 25,
+                  :two_pieces_owned => 50,
+                  :three_pieces_owned => 100 ,
+                  :four_pieces_owned => 200
                 }
      }
     }
